68e653f763965fadf790bc6043c66a1c9da268ec
Aryan Shah <shaharyand@gmail.com>
2025-03-03 12:56:34 +0530

    changes in config

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,16 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    EMAIL_HOST: process.env.EMAIL_HOST,
+    EMAIL_PORT: process.env.EMAIL_PORT,
+    EMAIL_SECURE: process.env.EMAIL_SECURE,
+    EMAIL_USER: process.env.EMAIL_USER,
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 }
 
 mergeConfig(nextConfig, userConfig)
